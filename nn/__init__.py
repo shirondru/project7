14f19e3456494930b0199a712de1ee7b1297faae
shirondru <59421145+shirondru@users.noreply.github.com>
2022-03-05 18:16:37 -0800

    Add docstring to init.py

--- nn/__init__.py
+++ nn/__init__.py
@@ -1,3 +1,8 @@
+"""
+BMI203: Biocomputing algorithms Winter 2022
+Assignment 7: Neural Networks
+"""
+
 from .nn import NeuralNetwork
 from .preprocess import one_hot_encode_seqs, sample_seqs
 from .io import read_text_file, read_fasta_file
