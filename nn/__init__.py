0305aca6d37de1faeee2e6e774d3e4d206ded006
shirondru <59421145+shirondru@users.noreply.github.com>
2022-03-06 21:39:39 -0800

    Re-implement to get better accuracy

--- nn/__init__.py
+++ nn/__init__.py
@@ -4,6 +4,6 @@ Assignment 7: Neural Networks
 """
 
 from .nn import NeuralNetwork
-from .preprocess import one_hot_encode_seqs, sample_seqs
+from .preprocess import one_hot_encode_seqs, sample_seqs, clip_sample_seqs
 from .io import read_text_file, read_fasta_file
 __version__ = "0.1.0"
